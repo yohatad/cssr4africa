5ff14590d47cde902e29ed109b6b430d9835bfb4
Yohannes Tadesse Haile <yohatad000@gmail.com>
2024-03-24 21:01:16 +0400

    moving to windows

--- cssr4africa/cssr_system/sound/src/Sound_detection.cpp
+++ cssr4africa/cssr_system/sound/src/Sound_detection.cpp
@@ -11,10 +11,61 @@
 #include "util.h"
 #include "sound.h"
 #include <vector>
+#include <ros/ros.h>
+
+std::string currentChannel = "rearLeft";
+const float speed_of_sound = 346;
+const float distance_between_ears = 0.07;
+const float sampling_rate = 48000;
+const int bufferSize = 4096;
+
+
+void Itd(float* data1, float* data2, int num_samples){     
+    
+    float ans[2 * bufferSize];
+    float sum_square_leftSound = 0;
+    float sum_square_rightSound = 0;
+
+    // Apply Energy Thresholding
+    for (int i = 0; i < num_samples; ++i) {
+        sum_square_leftSound += data1[i] * data1[i];
+        sum_square_rightSound += data2[i] * data2[i];
+    }
+
+    float threshold = 1000;
+    if (sum_square_leftSound < threshold || sum_square_rightSound < threshold) {
+        std::cout << "azimuth: 0 degree (Front)" << std::endl;
+        return;
+    }
+
+    correl(data1 - 1, data2 - 1, bufferSize, ans);
+
+    float max = ans[1];
+    int location = 1;
+    
+    for (int i = 2; i <= bufferSize; i++) {
+        if (max < ans[i]) {
+            max = ans[i];
+            location = i;
+        }
+    }
+
+    if (location != 1) {
+        num_samples = (location >= bufferSize / 2 + 1) ? (bufferSize + 1) - location : location - 1;
+        float ITD = num_samples / sampling_rate;
+        float z = ITD * (speed_of_sound / distance_between_ears);
+        double azimuth = std::asin(z) * (180.0 / M_PI);
+
+        std::cout << "azimuth: "<<azimuth << " degree " << ((location >= bufferSize / 2 + 1) ? "(Left)" : "(Right)");
+        std::cout << std::endl;
+    }
+    else {
+        std::cout << "azimuth: 0 degree (Front)" << std::endl;
+    }
+}
 
 void audiocallback(const naoqi_driver::AudioCustomMsg& msg) {
 
-    std::string currentChannel = "frontLeft";
     const std::vector<int16_t>* channelData = nullptr;
 
     if (currentChannel == "frontLeft") {
@@ -28,13 +79,21 @@ void audiocallback(const naoqi_driver::AudioCustomMsg& msg) {
     }
 
     // print channelData
-    for (int i = 0; i < channelData->size(); i++) {
-        std::cout << channelData->at(i) << std::endl;
-    }
-    
+    // for (int i = 0; i < channelData->size(); i++) {
+    //     std::cout << channelData->at(i) << std::endl;
+    // }
 
-    std::cout << "Received audio message" << std::endl;
+    // print the size of the channelData
+    // std::cout << channelData->size() << std::endl;
+
+    // create a buffer to store the data
+    float data[bufferSize];
+    data1 = new float[bufferSize];
+
+    
 }
+
+
 
 
 int main (int argc, char *argv[]){
@@ -47,65 +106,4 @@ int main (int argc, char *argv[]){
     ros::spin();
     return 0;
 }
-
-
-    
-    // // Constants
-    // const float speed_of_sound = 346;
-    // const float distance_between_ears = 0.09;
-    // const float sampling_rate = 48000;
-    // const unsigned long n = 16384;
-    
-    // float ans[2 * n];                       // Cross-correlation function
-    // float sum_square_leftSound = 0;
-    // float sum_square_rightSound = 0;
-    
-    // // File paths
-    // int num_samples = std::min(inputFileInfo.frames, inputFileInfo2.frames);
-    // float data1[num_samples];
-    // float data2[num_samples];
-
-    // sf_read_float(inputFile1, data1, num_samples);
-    // sf_read_float(inputFile2, data2, num_samples);
-
-    // // Apply Energy Thresholding
-    // for (int i = 0; i < num_samples; ++i) {
-    //     sum_square_leftSound += data1[i] * data1[i];
-    //     sum_square_rightSound += data2[i] * data2[i];
-    // }
-
-    // float threshold = 1000;
-    // if (sum_square_leftSound < threshold || sum_square_rightSound < threshold) {
-    //     std::cout << "azimuth: 0 degree (Front)" << std::endl;
-    //     return 0;
-    // }
-
-    // }   
-
-    // correl(data1 - 1, data2 - 1, n, ans);
-
-    // float max = ans[1];
-    // int location = 1;
-    // for (int i = 2; i <= n; i++) {
-    //     if (max < ans[i]) {
-    //         max = ans[i];
-    //         location = i;
-    //     }
-    // }
-
-    // if (location != 1) {
-    //     num_samples = (location >= n / 2 + 1) ? (n + 1) - location : location - 1;
-    //     float ITD = num_samples / sampling_rate;
-    //     float z = ITD * (speed_of_sound / distance_between_ears);
-    //     double azimuth = std::asin(z) * (180.0 / M_PI);
-
-    //     std::cout << "azimuth: "<<azimuth << " degree " << ((location >= n / 2 + 1) ? "(Left)" : "(Right)");
-    //     std::cout << std::endl;
-    // }
-    // else {
-    //     std::cout << "azimuth: 0 degree (Front)" << std::endl;
-    // }
-
-
-    // return 0;
 
