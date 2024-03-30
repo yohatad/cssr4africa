4236deafee773cee80bc3453ab33bf86746e3846
yohtad <yohatad000@gmail.com>
2024-03-30 17:10:31 +0200

    Edited the sound detection

--- cssr4africa/cssr_system/sound/src/Sound_detection.cpp
+++ cssr4africa/cssr_system/sound/src/Sound_detection.cpp
@@ -13,21 +13,19 @@
 #include <vector>
 #include <ros/ros.h>
 
-std::string currentChannel = "rearLeft";
 const float speed_of_sound = 346;
 const float distance_between_ears = 0.07;
 const float sampling_rate = 48000;
 const int bufferSize = 4096;
 
-
-void Itd(float* data1, float* data2, int num_samples){     
+void Itd(float data1[], float data2[]){
     
     float ans[2 * bufferSize];
     float sum_square_leftSound = 0;
     float sum_square_rightSound = 0;
 
     // Apply Energy Thresholding
-    for (int i = 0; i < num_samples; ++i) {
+    for (int i = 0; i < bufferSize; i++) {
         sum_square_leftSound += data1[i] * data1[i];
         sum_square_rightSound += data2[i] * data2[i];
     }
@@ -38,6 +36,7 @@ void Itd(float* data1, float* data2, int num_samples){
         return;
     }
 
+    // correl(data1, data2, bufferSize, ans);
     correl(data1 - 1, data2 - 1, bufferSize, ans);
 
     float max = ans[1];
@@ -51,7 +50,7 @@ void Itd(float* data1, float* data2, int num_samples){
     }
 
     if (location != 1) {
-        num_samples = (location >= bufferSize / 2 + 1) ? (bufferSize + 1) - location : location - 1;
+        int num_samples = (location >= bufferSize / 2 + 1) ? (bufferSize + 1) - location : location - 1;
         float ITD = num_samples / sampling_rate;
         float z = ITD * (speed_of_sound / distance_between_ears);
         double azimuth = std::asin(z) * (180.0 / M_PI);
@@ -63,38 +62,25 @@ void Itd(float* data1, float* data2, int num_samples){
         std::cout << "azimuth: 0 degree (Front)" << std::endl;
     }
 }
-
 void audiocallback(const naoqi_driver::AudioCustomMsg& msg) {
 
-    const std::vector<int16_t>* channelData = nullptr;
+    const std::vector<int16_t>* frontLeft = nullptr;
+    const std::vector<int16_t>* frontRight = nullptr;
+    
+    frontLeft   = &msg.frontLeft;
+    frontRight  = &msg.frontRight;
 
-    if (currentChannel == "frontLeft") {
-        channelData = &msg.frontLeft;
-    } else if (currentChannel == "frontRight") {
-        channelData = &msg.frontRight;
-    } else if (currentChannel == "rearLeft") {
-        channelData = &msg.rearLeft;
-    } else if (currentChannel == "rearRight") {
-        channelData = &msg.rearRight;
+    float data1[bufferSize];
+    float data2[bufferSize];
+
+    for (int i = 0; i < bufferSize; i++) {
+        data1[i] = frontLeft->at(i);
+        data2[i] = frontRight->at(i);
     }
 
-    // print channelData
-    // for (int i = 0; i < channelData->size(); i++) {
-    //     std::cout << channelData->at(i) << std::endl;
-    // }
-
-    // print the size of the channelData
-    // std::cout << channelData->size() << std::endl;
-
-    // create a buffer to store the data
-    float data[bufferSize];
-    data1 = new float[bufferSize];
-
-    
+    // perform ITD
+    Itd(data1, data2);
 }
-
-
-
 
 int main (int argc, char *argv[]){
     ros::init (argc, argv, "sound_localization");
