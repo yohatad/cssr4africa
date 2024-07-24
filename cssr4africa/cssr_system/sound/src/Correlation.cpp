6f56e18b26bbbf38acd1c9baf3f511a5bc2becc7
yohtad <yohatad000@gmail.com>
2024-07-24 12:26:45 +0200

    Code for Testing Directional checking

--- cssr4africa/cssr_system/sound/src/Correlation.cpp
+++ cssr4africa/cssr_system/sound/src/Correlation.cpp
@@ -51,87 +51,83 @@
 *************************************************************************************************/
 
 #include "util.h"
+#include <vector>
+#include <cmath>
+
+void four1(float data[], unsigned long nn, int isign);
+void realft(float data[], unsigned long n, int isign);
+void twofft(const float data1[], const float data2[], float fft1[], float fft2[], unsigned long n);
 
 void correl(const float data1[], const float data2[], unsigned long n, float ans[])
 {
-    void realft(float data[], unsigned long n, int isign);
-    void twofft(const float data1[],const float data2[], float fft1[], float fft2[], unsigned long n);
-    
-    unsigned long no2, i;
-    float dum, *fft;
+    unsigned long no2 = n >> 1;
+    std::vector<float> fft(n << 1);
 
-    fft = vector(1, n << 1); // Allocate memory for FFT arrays
-    
-    twofft(data1, data2, fft, ans, n); // Transform both data vectors at once
-    
-    no2 = n >> 1; // Normalization factor for inverse FFT
-    
-    for (i = 2; i <= n + 2; i += 2) {
-        ans[i - 1] = (fft[i - 1] * (dum = ans[i - 1]) + fft[i] * ans[i]) / no2; // Multiply to find FFT of their correlation
-        ans[i] = (fft[i] * dum - fft[i - 1] * ans[i]) / no2;
+    // Perform FFT on both data vectors
+    twofft(data1, data2, fft.data(), ans, n);
+
+    // Multiply to find FFT of their correlation
+    for (unsigned long i = 2; i <= n + 2; i += 2) {
+        float temp = ans[i - 1];
+        ans[i - 1] = (fft[i - 1] * temp + fft[i] * ans[i]) / no2;
+        ans[i] = (fft[i] * temp - fft[i - 1] * ans[i]) / no2;
     }
-    
-    ans[2] = ans[n + 1]; // Pack first and last elements into one
-    
-    realft(ans, n, -1); // Inverse transform gives correlation
-    
-    free_vector(fft, 1, n << 1); // Free memory used for FFT arrays
+
+    // Pack first and last elements into one
+    ans[2] = ans[n + 1];
+
+    // Inverse transform gives correlation
+    realft(ans, n, -1);
 }
 
-
-void twofft(const float data1[],const float data2[], float fft1[], float fft2[], unsigned long n)
+void twofft(const float data1[], const float data2[], float fft1[], float fft2[], unsigned long n)
 {
-    void four1(float data[], unsigned long nn, int isign);
-    unsigned long nn3, nn2, jj, j;
-    float rep, rem, aip, aim;
-
-    nn3 = 1 + (nn2 = 2 + n + n); // Calculate sizes for intermediate arrays
+    unsigned long nn3 = 1 + (2 + n + n); // Calculate sizes for intermediate arrays
 
     // Pack the two real arrays into one complex array
-    for (j = 1, jj = 2; j <= n; j++, jj += 2) {
+    for (unsigned long j = 1, jj = 2; j <= n; j++, jj += 2) {
         fft1[jj - 1] = data1[j];
         fft1[jj] = data2[j];
     }
 
-    four1(fft1, n, 1); // Transform the complex array using FFT
+    // Transform the complex array using FFT
+    four1(fft1, n, 1);
 
     fft2[1] = fft1[2];
     fft1[2] = fft2[2] = 0.0;
 
     // Use symmetries to separate the two transformed forms
-    for (j = 3; j <= n + 1; j += 2) {
-        rep = 0.5 * (fft1[j] + fft1[nn2 - j]); // Real part
-        rem = 0.5 * (fft1[j] - fft1[nn2 - j]); // Real part
-        aip = 0.5 * (fft1[j + 1] + fft1[nn3 - j]); // Imaginary part
-        aim = 0.5 * (fft1[j + 1] - fft1[nn3 - j]); // Imaginary part
-        
-        // Ship them out in two complex arrays
+    for (unsigned long j = 3; j <= n + 1; j += 2) {
+        float rep = 0.5f * (fft1[j] + fft1[nn3 - j]);
+        float rem = 0.5f * (fft1[j] - fft1[nn3 - j]);
+        float aip = 0.5f * (fft1[j + 1] + fft1[nn3 - j]);
+        float aim = 0.5f * (fft1[j + 1] - fft1[nn3 - j]);
+
         fft1[j] = rep;
         fft1[j + 1] = aim;
-        fft1[nn2 - j] = rep;
+        fft1[nn3 - j] = rep;
         fft1[nn3 - j] = -aim;
         fft2[j] = aip;
         fft2[j + 1] = -rem;
-        fft2[nn2 - j] = aip;
+        fft2[nn3 - j] = aip;
         fft2[nn3 - j] = rem;
     }
 }
 
 void realft(float data[], unsigned long n, int isign)
 {
-    void four1(float data[], unsigned long nn, int isign);
     unsigned long i, i1, i2, i3, i4, np3;
     float c1 = 0.5, c2, h1r, h1i, h2r, h2i;
     double wr, wi, wpr, wpi, wtemp, theta;
 
-    theta = 3.141592653589793 / (double)(n >> 1); // Initialize the trigonometric recurrence
+    theta = M_PI / (double)(n >> 1);
 
     if (isign == 1) {
         c2 = -0.5;
-        four1(data, n >> 1, 1); // Perform the forward transform
+        four1(data, n >> 1, 1);
     } else {
         c2 = 0.5;
-        theta = -theta; // Set up for an inverse transform
+        theta = -theta;
     }
 
     wtemp = sin(0.5 * theta);
@@ -141,7 +137,6 @@ void realft(float data[], unsigned long n, int isign)
     wi = wpi;
     np3 = n + 3;
 
-    // Separate the two transforms and recombine them to get the true transform of the original real data
     for (i = 2; i <= (n >> 2); i++) {
         i4 = 1 + (i3 = np3 - (i2 = 1 + (i1 = i + i - 1)));
         h1r = c1 * (data[i1] + data[i3]);
@@ -152,40 +147,33 @@ void realft(float data[], unsigned long n, int isign)
         data[i2] = h1i + wr * h2i + wi * h2r;
         data[i3] = h1r - wr * h2r + wi * h2i;
         data[i4] = -h1i + wr * h2i + wi * h2r;
-        wr = (wtemp = wr) * wpr - wi * wpi + wr; // Trigonometric recurrence
+        wr = (wtemp = wr) * wpr - wi * wpi + wr;
         wi = wi * wpr + wtemp * wpi + wi;
     }
 
     if (isign == 1) {
-        data[1] = (h1r = data[1]) + data[2]; // Squeeze the first and last data components together
+        data[1] = (h1r = data[1]) + data[2];
         data[2] = h1r - data[2];
     } else {
         data[1] = c1 * ((h1r = data[1]) + data[2]);
         data[2] = c1 * (h1r - data[2]);
-        four1(data, n >> 1, -1); // Perform the inverse transform for the case isign=-1
+        four1(data, n >> 1, -1);
     }
 }
 
+#define SWAP(a, b) { float temp = (a); (a) = (b); (b) = temp; }
 
-
-#define SWAP(a, b) tempr = (a); (a) = (b); (b) = tempr
 void four1(float data[], unsigned long nn, int isign)
 {
-    unsigned long n, mmax, m, j, istep, i;
-    double wtemp, wr, wpr, wpi, wi, theta;
-    float tempr, tempi;
+    unsigned long n = nn << 1;
+    unsigned long j = 1;
 
-    n = nn << 1; // Calculate the total data length
-
-    j = 1;
-
-    // Reorder the data using bit reversal
-    for (i = 1; i < n; i += 2) {
+    for (unsigned long i = 1; i < n; i += 2) {
         if (j > i) {
-            SWAP(data[j], data[i]); // Exchange the two complex numbers
+            SWAP(data[j], data[i]);
             SWAP(data[j + 1], data[i + 1]);
         }
-        m = nn;
+        unsigned long m = nn;
         while (m >= 2 && j > m) {
             j -= m;
             m >>= 1;
@@ -193,36 +181,29 @@ void four1(float data[], unsigned long nn, int isign)
         j += m;
     }
 
-    mmax = 2;
+    unsigned long mmax = 2;
+    while (n > mmax) {
+        unsigned long istep = mmax << 1;
+        double theta = isign * (2.0 * M_PI / mmax);
+        double wtemp = sin(0.5 * theta);
+        double wpr = -2.0 * wtemp * wtemp;
+        double wpi = sin(theta);
+        double wr = 1.0;
+        double wi = 0.0;
 
-    // Apply the Danielson-Lanczos section of the routine
-    while (n > mmax) { // Outer loop executed log2 nn times
-        istep = mmax << 1;
-        theta = isign * (6.28318530717959 / mmax); // Initialize the trigonometric recurrence
-        wtemp = sin(0.5 * theta);
-        wpr = -2.0 * wtemp * wtemp;
-        wpi = sin(theta);
-        wr = 1.0;
-        wi = 0.0;
-
-        // Nested inner loops for the Danielson-Lanczos formula
-        for (m = 1; m < mmax; m += 2) {
-            for (i = m; i <= n; i += istep) {
+        for (unsigned long m = 1; m < mmax; m += 2) {
+            for (unsigned long i = m; i <= n; i += istep) {
                 j = i + mmax;
-                // Danielson-Lanczos formula
-                tempr = wr * data[j] - wi * data[j + 1];
-                tempi = wr * data[j + 1] + wi * data[j];
+                float tempr = wr * data[j] - wi * data[j + 1];
+                float tempi = wr * data[j + 1] + wi * data[j];
                 data[j] = data[i] - tempr;
                 data[j + 1] = data[i + 1] - tempi;
                 data[i] += tempr;
                 data[i + 1] += tempi;
             }
-            wr = (wtemp = wr) * wpr - wi * wpi + wr; // Trigonometric recurrence
+            wr = (wtemp = wr) * wpr - wi * wpi + wr;
             wi = wi * wpr + wtemp * wpi + wi;
         }
         mmax = istep;
     }
 }
-
-
-
--- cssr4africa/cssr_system/sound/src/Sound_detection.cpp
+++ cssr4africa/cssr_system/sound/src/Sound_detection.cpp
@@ -17,20 +17,24 @@
 #include <fstream>
 
 const float SPEED_OF_SOUND = 346.0f;
-const float DISTANCE_BETWEEN_EARS = 0.07f;
+const float DISTANCE_BETWEEN_EARS = 0.0686f;
 const float SAMPLING_RATE = 48000.0f;
 const int TARGET_BUFFER_SIZE = 8192;  // New target buffer size for accumulation
 const int WINDOW_SIZE = 1;  // Number of values to consider for the mode
 const float INTENSITY_THRESHOLD = 400;
 
-std::vector<double> angle_values;  // Vector to store ITD angle values
+std::vector<double> angle_values;            // Vector to store ITD angle values
 std::vector<int16_t> accumulated_frontLeft;  // Accumulated data for front left mic
 std::vector<int16_t> accumulated_frontRight; // Accumulated data for front right mic
+std::vector<int16_t> accumulated_rearLeft;   // Accumulated data for back left mic
+std::vector<int16_t> accumulated_rearRight;  // Accumulated data for back right mic
 
 double calculateItd(const float* data1, const float* data2, int size);
 float calculateRms(const std::vector<int16_t>& data);
 void audioCallback(const naoqi_driver::AudioCustomMsg& msg);
 double calculateMode(const std::vector<double>& values);
+double calculateSoundDirection(const std::vector<float>& data1, const std::vector<float>& data2,
+                               const std::vector<float>& data3, const std::vector<float>& data4);
 
 // Declare the publisher as a global variable
 ros::Publisher sound_direction_pub;
@@ -61,13 +65,52 @@ float calculateRms(const std::vector<int16_t>& data) {
 void audioCallback(const naoqi_driver::AudioCustomMsg& msg) {
     const std::vector<int16_t>& frontLeft = msg.frontLeft;
     const std::vector<int16_t>& frontRight = msg.frontRight;
+    const std::vector<int16_t>& rearLeft = msg.rearLeft;
+    const std::vector<int16_t>& rearRight = msg.rearRight;
 
     accumulated_frontLeft.insert(accumulated_frontLeft.end(), frontLeft.begin(), frontLeft.end());
     accumulated_frontRight.insert(accumulated_frontRight.end(), frontRight.begin(), frontRight.end());
+    accumulated_rearLeft.insert(accumulated_rearLeft.end(), rearLeft.begin(), rearLeft.end());
+    accumulated_rearRight.insert(accumulated_rearRight.end(), rearRight.begin(), rearRight.end());
+    
 
     if (accumulated_frontLeft.size() >= TARGET_BUFFER_SIZE) {
         std::vector<float> data1(accumulated_frontLeft.begin(), accumulated_frontLeft.end());
         std::vector<float> data2(accumulated_frontRight.begin(), accumulated_frontRight.end());
+        std::vector<float> data3(accumulated_rearLeft.begin(), accumulated_rearLeft.end());
+        std::vector<float> data4(accumulated_rearRight.begin(), accumulated_rearRight.end());
+
+        std::vector<float> flfr(2 * TARGET_BUFFER_SIZE, 0.0f);
+        std::vector<float> flbl(2 * TARGET_BUFFER_SIZE, 0.0f);
+        std::vector<float> frbr(2 * TARGET_BUFFER_SIZE, 0.0f);
+
+        correl(data1.data(), data2.data(), TARGET_BUFFER_SIZE, flfr.data());
+        correl(data1.data(), data3.data(), TARGET_BUFFER_SIZE, flbl.data());
+        correl(data2.data(), data4.data(), TARGET_BUFFER_SIZE, frbr.data());
+
+        auto max_itFLFR = std::max_element(flfr.begin() + 1, flfr.begin() + TARGET_BUFFER_SIZE + 1);
+        int locationFLFR = std::distance(flfr.begin(), max_itFLFR);
+        
+        if (locationFLFR != 1) {
+            int num_samples = (locationFLFR >= TARGET_BUFFER_SIZE / 2 + 1) ? (TARGET_BUFFER_SIZE + 1 - locationFLFR) : (locationFLFR - 1);
+            std::cout<<"num_samples FLFR: "<<num_samples<<std::endl;
+        }
+
+        auto max_itFLBL = std::max_element(flbl.begin() + 1, flbl.begin() + TARGET_BUFFER_SIZE + 1);
+        int locationFLBL = std::distance(flbl.begin(), max_itFLBL);
+        
+        if (locationFLFR != 1) {
+            int num_samples = (locationFLBL >= TARGET_BUFFER_SIZE / 2 + 1) ? (TARGET_BUFFER_SIZE + 1 - locationFLBL) : (locationFLBL - 1);
+            std::cout<<"num_samples FLBL: "<<num_samples<<std::endl;
+        }
+
+        auto max_itFRBR = std::max_element(frbr.begin() + 1, frbr.begin() + TARGET_BUFFER_SIZE + 1);
+        int locationFRBR = std::distance(frbr.begin(), max_itFRBR);
+
+        if (locationFLFR != 1) {
+            int num_samples = (locationFRBR >= TARGET_BUFFER_SIZE / 2 + 1) ? (TARGET_BUFFER_SIZE + 1 - locationFRBR) : (locationFRBR - 1);
+            std::cout<<"num_samples FRBR: "<<num_samples<<std::endl;
+        }
 
         float rmsFrontLeft = calculateRms(accumulated_frontLeft);
         float rmsFrontRight = calculateRms(accumulated_frontRight);
