4078cc19010eded84faf5d9cd776a3eb5f4f48ab
Yohannes Haile <yohatad000@gmail.com>
2025-03-16 15:24:37 +0200

    Updated description of the code

--- cssr4africa/cssr_system/face_detection/src/face_detection_application.py
+++ cssr4africa/cssr_system/face_detection/src/face_detection_application.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-face_detection_application.py Application code to run the face and mutual gaze detection algorithm.
+face_detection_application.py Application code to run the Face and Mutual Gaze Detection and Localization ROS node.
 
 Author: Yohannes Tadesse Haile
 Date: March 15, 2025
@@ -20,7 +20,7 @@ This program comes with ABSOLUTELY NO WARRANTY.
 """
 face_detection_application.py   Application code to run the face and mutual gaze detection algorithm.
 
-The face detectionis implemented using the ROS image topic that could be configured to be the intel realsense camera or pepper robot
+The face detection is implemented using the ROS image topic that could be configured to be the intel realsense camera or pepper robot
 camera. It uses OpenCV to visualize the detected faces and gaze direction. The gaze direction is calculated using face
 mesh landmarks which uses Google's MediaPipe library. The media pipe utilizes CPU for face detection and gaze direction.
 The SixDrepNet uses YOLOONNX for face detection and SixDrepNet for gaze direction. The SixDrepNet utilizes GPU for faster
@@ -40,41 +40,56 @@ Libraries
     - os: Python library for operating system operations
     - onnxruntime: ONNX Runtime library for deep learning model inference
     - multiprocessing: Python library for multiprocessing operations
-    - collections: Python library for collection operations
-    - scipy.spatial: SciPy library for spatial operations
-    - distance: SciPy library for distance operations
-    - Point: ROS geometry_msgs library for Point operations
-    - faceDetection: ROS face_detection library for face detection operations
+    - json: Python library for parsing and handling JSON data.
+    - random: Python library for generating random numbers or selections.
+    - math (cos, sin, pi): Python library providing mathematical functions, specifically cosine, sine, and pi constants.
+    - sensor_msgs.msg (Image, CompressedImage): ROS message types for handling standard and compressed image data.
+    - cv_bridge (CvBridge, CvBridgeError): ROS library used for converting ROS image messages to OpenCV images and vice versa.
+    - geometry_msgs.msg (Point): ROS message type for representing points in 3D space.
+    - typing (Tuple, List): Python library to support type hints for improved code clarity.
+    - face_detection.msg (msg_file): Custom ROS message from face_detection package for handling face detection information.
+    - face_detection_tracking (Sort, CentroidTracker): Custom implementation for object tracking (SORT algorithm and centroid-based tracker).
 
 Parameters
-    Command line arguments: None
+    Command line arguments: 
+        Launch File Parameters:
+            roslaunch face_detection face_detection_robot.launch camera:=realsense 
+                camera: Camera type (realsense or pepper)
+                pepper_robot_ip: Pepper robot IP address
+                network_interface: Network interface for Pepper robot connection
+
 
     Configuration File Parameters
         Key                             Value
-        camera                          realsense
-        algorithm                       mediapipe
-        centroid_max_distance           15
-        centroid_max_disappeared        100
+        algorithm                       sixdrep
+        use_compressed                  true
         mp_facedet_confidence           0.5
         mp_headpose_angle               5
+        centroid_max_distance           15
+        centroid_max_disappeared        100
         sixdrepnet_confidence           0.65
         sixdrepnet_headpose_angle       10
-        verboseMode                     True
+        sort_max_disappeared            30
+        sort_min_hits                   20
+        sort_iou_threshold              0.3
+        verbose_mode                    true
 
 Subscribed Topics
-    Topic Name                              Message Type
-    /camera/color/image_raw                 sensor_msgs/Image              
-    /camera/depth/image_rect_raw            sensor_msgs/Image
-    /naoqi_driver/camera/front/image_raw    sensor_msgs/Image
-    /naoqi_driver/camera/depth/image_raw    sensor_msgs/Image
+    Topic Name                                  Message Type
+    /camera/color/image_raw                     sensor_msgs/Image              
+    /camera/aligned_depth_to_color/image_raw    sensor_msgs/Image
+    /naoqi_driver/camera/front/image_raw        sensor_msgs/Image
+    /naoqi_driver/camera/depth/image_raw        sensor_msgs/Image
 
 Published Topics
-    Topic Name                              Message Type
-    /faceDetection/data                     face_detection/face_detection.msg
+    Topic Name                                  Message Type
+    /faceDetection/data                         face_detection/face_detection.msg
 
 Input Data Files
     - faceDetectionConfiguration.json: Configuration file for face detection parameters
     - pepperTopics.dat: Data file for Pepper robot camera topics
+
+Model Files
     - face_detection_YOLO.onnx: YOLOONNX model for face detection
     - face_detection_sixdrepnet360.onnx: SixDrepNet model for gaze direction
 
